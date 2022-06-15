25f151d8585bcbd0d42dd4f0996e64c1be782ecb
gg_tranghp <tranghnp@greenglobal.vn>
2022-06-15 21:35:22 +0700

    migration

---
+++ host/Datn.ApiManagement.HttpApi.Host/Migrations/20220615143504_Added_PaypalInfor_For_UserTransaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Datn.ApiManagement.Migrations
+{
+    public partial class Added_PaypalInfor_For_UserTransaction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PayerIDPayPalId",
+                schema: "transaction",
+                table: "UserTransactions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "PaymenPayPalId",
+                schema: "transaction",
+                table: "UserTransactions",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PayerIDPayPalId",
+                schema: "transaction",
+                table: "UserTransactions");
+
+            migrationBuilder.DropColumn(
+                name: "PaymenPayPalId",
+                schema: "transaction",
+                table: "UserTransactions");
+        }
+    }
+}
